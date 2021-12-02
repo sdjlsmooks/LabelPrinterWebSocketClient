10a0859ad6da3dc2422557cc853a91942c71f892
sdjl@qwestoffice.net <sdjl@qwestoffice.net>
2021-12-02 16:02:00 -0700

    Going back to 1 label a person.
    Had to decompile jar since lost the original source code.

--- src/com/sdjl/PrintPassZebraJob.java
+++ src/com/sdjl/PrintPassZebraJob.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
@@ -16,101 +17,88 @@ import java.text.SimpleDateFormat;
 import java.util.Base64;
 import java.util.Calendar;
 import java.util.Date;
-
 import javax.imageio.ImageIO;
-
 import org.json.simple.JSONObject;
 
 class PrintPassZebraJob implements Printable {
-	
-	  private JSONObject jo;
-	
-	  private static Font largeBoldFont = new Font("Calibri", Font.BOLD, 35);
-	  private static Font smallPlainFont = new Font("Calibri", Font.PLAIN, 15);
-	  //private static Font sFont = new Font("Calibri", Font.PLAIN, 12);
+   private JSONObject jo;
+   private static Font largeBoldFont = new Font("Calibri", 1, 35);
+   private static Font smallPlainFont = new Font("Calibri", 0, 15);
 
-		BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
-		    BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
-		    Graphics2D graphics2D = resizedImage.createGraphics();
-		    graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
-		    graphics2D.dispose();
-		    return resizedImage;
-		}
-	  
-	  public int print(Graphics g, PageFormat pf, int page) throws PrinterException {
-	    if (page > 1) {
-	      return NO_SUCH_PAGE;
-	    }
-	    else if (page == 1) {
-	    	Graphics2D g2 = (Graphics2D)g;
-	    	g2.setFont(smallPlainFont);
-	    	g2.drawString("INTENTIONALLY BLANK",10,40);
-	    	return PAGE_EXISTS;
-	    }
-    	try {	    	
-    		Graphics2D g2 = (Graphics2D) g;
-				
-    		g2.setFont(largeBoldFont);
-    		g2.setPaint(Color.black);
-    		int strokeThickness = 7;
-    		Stroke oldStroke = g2.getStroke();
-    		g2.setStroke(new BasicStroke(strokeThickness));
-    		g2.drawRoundRect(10, 10, 200, 60, 10,10);
+   BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+      BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, 1);
+      Graphics2D graphics2D = resizedImage.createGraphics();
+      graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, (ImageObserver)null);
+      graphics2D.dispose();
+      return resizedImage;
+   }
 
-    		JSONObject data = (JSONObject)jo.get("data");
-    		Double temperature = 0.0;
+   public int print(Graphics g, PageFormat pf, int page) throws PrinterException {
+      try {
+         if (page > 1) {
+            return 1;
+         }
 
-		
-			// Print picture
-			String imageString = (String)data.get("signAvatar");
-			
-			if (imageString != null) {					
-					byte[] imageBytes = Base64.getDecoder().decode(imageString);
-					BufferedImage img = ImageIO.read(new ByteArrayInputStream(imageBytes));									
-					img = resizeImage(img, 60, 60);
-					g.drawImage(img, 15, 10,  null);
-			}	    
-			
-			
-			// Retrieve Temperature
-			// typecasting obj to JSONObject 
-			// NOTE - The scanned photo from the device is in here.  Use
-			//        it or the original file contents for the label.
-			if (data != null) {
-				if (data.get("temperature") instanceof Long) {
-					temperature = ((Long)(data.get("temperature"))).doubleValue();	
-				}
-				else {
-					temperature = (Double)(data.get("temperature"));
-				}
-			}
-			
-			
-			// Print out today + temperature
-			Calendar todayCal = Calendar.getInstance();
-			Date today = todayCal.getTime();
-			SimpleDateFormat formatter=new SimpleDateFormat("MM/dd/yyyy");
-						
-			Font oldFont = g2.getFont();
-			g2.setFont(smallPlainFont);
-			g.drawString("Dt: "+formatter.format(today), 75, 25);		
-			g.drawString("Temp: "+((double)Math.round(temperature*10))/10,75,37);
-			
-			
-			g2.setFont(largeBoldFont);
-			g2.drawString("PASS", 75, 65);
-			g2.setStroke(oldStroke);
-			g2.setFont(oldFont);
-	    	
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    return PAGE_EXISTS;
-	  }
-	  
-	  PrintPassZebraJob(JSONObject objectToPrint) {
-		  jo = objectToPrint;
-	  }
+         if (page == 1) {
+            //this.printLabel(g);
+        	this.printIntentionallyBlank(g);
+            return 0;
+         }
 
+         this.printLabel(g);
+      } catch (IOException var5) {
+         var5.printStackTrace();
+      }
+
+      return 0;
+   }
+
+   private void printIntentionallyBlank(Graphics g) {
+      Graphics2D g2 = (Graphics2D)g;
+      g2.setFont(smallPlainFont);
+      g2.drawString("INTENTIONALLY BLANK", 10, 40);
+   }
+
+   private void printLabel(Graphics g) throws IOException {
+      Graphics2D g2 = (Graphics2D)g;
+      g2.setFont(largeBoldFont);
+      g2.setPaint(Color.black);
+      int strokeThickness = 7;
+      Stroke oldStroke = g2.getStroke();
+      g2.setStroke(new BasicStroke((float)strokeThickness));
+      g2.drawRoundRect(10, 10, 200, 60, 10, 10);
+      JSONObject data = (JSONObject)this.jo.get("data");
+      Double temperature = 0.0D;
+      String imageString = (String)data.get("signAvatar");
+      if (imageString != null) {
+         byte[] imageBytes = Base64.getDecoder().decode(imageString);
+         BufferedImage img = ImageIO.read(new ByteArrayInputStream(imageBytes));
+         img = this.resizeImage(img, 60, 60);
+         g.drawImage(img, 15, 10, (ImageObserver)null);
+      }
+
+      if (data != null) {
+         if (data.get("temperature") instanceof Long) {
+            temperature = ((Long)data.get("temperature")).doubleValue();
+         } else {
+            temperature = (Double)data.get("temperature");
+         }
+      }
+
+      Calendar todayCal = Calendar.getInstance();
+      Date today = todayCal.getTime();
+      SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+      Font oldFont = g2.getFont();
+      g2.setFont(smallPlainFont);
+      g.drawString("Dt: " + formatter.format(today), 75, 25);
+      g.drawString("Temp: " + (double)Math.round(temperature * 10.0D) / 10.0D, 75, 37);
+      g2.setFont(largeBoldFont);
+      g2.drawString("PASS", 75, 65);
+      g2.setStroke(oldStroke);
+      g2.setFont(oldFont);
+   }
+
+   PrintPassZebraJob(JSONObject objectToPrint) {
+      this.jo = objectToPrint;
+   }
 }
--- src/com/sdjl/WSClient.java
+++ src/com/sdjl/WSClient.java
@@ -15,13 +15,12 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.jws.WebService;
+import javax.print.DocFlavor;
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
+import javax.print.attribute.AttributeSet;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.PrintRequestAttributeSet;
-import javax.print.attribute.ResolutionSyntax;
 import javax.print.attribute.standard.OrientationRequested;
 import javax.print.attribute.standard.PrinterResolution;
 import javax.websocket.ClientEndpoint;
@@ -29,7 +28,6 @@ import javax.websocket.ContainerProvider;
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
-
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -40,200 +38,171 @@ import org.json.simple.parser.ParseException;
 
 @ClientEndpoint
 public class WSClient {
-	private static Object waitLock = new Object();
-	
-	
-	private static String printer="Dymo";
-	
-	
-	@OnMessage
-	public void onMessage(String message) {
-		// The raw data received from the temperature scanner
-		System.out.println("-------------------------------->>>>>>");
-		String timeStamp = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(Calendar.getInstance().getTime());
-		System.out.println("Date: " + timeStamp + " Received msg: ");
-		System.out.println(message);
-		
-		// Parse out the JSON from device.
-		try {
-			Object obj = new JSONParser().parse(new StringReader(message));
-			JSONObject jo = (JSONObject) obj;			
-			if (jo != null) {
-				JSONObject dataObject = (JSONObject)jo.get("data");
-				if (dataObject != null) {
-					Double temperature = 0.0;
-					if (dataObject.get("temperature") instanceof Long) {
-						Long tempLong = (Long)dataObject.get("temperature");
-						temperature = tempLong.doubleValue();
-					}
-					else {
-						temperature = (Double)dataObject.get("temperature");
-					}
-					// wearMask == 1 --> TRUE (wearing mask)
-					// wearMask == 0 --> FALSE (not wearing mask)
-					//Long wearMask = (Long)dataObject.get("wearMask");
-					//if (wearMask == 1) { // turn off for now, the mask detection on the device does not work very well.
-						if ((temperature != null) && (temperature >= 96.8 ) && (temperature <= 99.5)) {
-							// Print pass
-							printPass(jo);
-						}
-					//}
-				}
-			}
-		} catch (IOException | ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+   private static Object waitLock = new Object();
+   private static String printer = "Dymo";
 
-	
-	private static void wait4TerminateSignal() {
-		synchronized (waitLock) {
-			try {
-				waitLock.wait();
-			} catch (InterruptedException e) {
-			}
-		}
-	}
+   @OnMessage
+   public void onMessage(String message) {
+      System.out.println("-------------------------------->>>>>>");
+      String timeStamp = (new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")).format(Calendar.getInstance().getTime());
+      System.out.println("Date: " + timeStamp + " Received msg: ");
+      System.out.println(message);
 
-	private void printPass(JSONObject jo) {
-		try {
-			
-			PrinterJob job = PrinterJob.getPrinterJob();
-		    PageFormat pf = job.defaultPage();
-		    Paper paper = pf.getPaper();
-			Printable passable = new PrintPassJob2(jo);
-			PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
-			boolean zdPrinter = printer.contains("ZD420");
-			if (!zdPrinter) {
-				passable = new PrintPassJob2(jo);		
-			    paper.setSize(1.0 * 72, 3.47 * 72);
-			    paper.setImageableArea(0.1 * 72, 0.1 * 72, 1.0 * 72, 3.47 * 72);					
-				attributes.add(OrientationRequested.LANDSCAPE);
-				PrinterResolution pr = new PrinterResolution(313, 313, ResolutionSyntax.DPI);
-				attributes.add(pr);
-			}
-			else {
-				passable = new PrintPassZebraJob(jo);
-			    paper.setSize((3.0*72), (1.12 * 72));
-			    paper.setImageableArea(0, 0, 3.0 * 72, 1.00 * 72);
-				PrinterResolution pr = new PrinterResolution(203, 203, ResolutionSyntax.DPI);
-				attributes.add(OrientationRequested.PORTRAIT);
-				attributes.add(pr);
-			}
+      try {
+         Object obj = (new JSONParser()).parse(new StringReader(message));
+         JSONObject jo = (JSONObject)obj;
+         if (jo != null) {
+            JSONObject dataObject = (JSONObject)jo.get("data");
+            if (dataObject != null) {
+               Double temperature = 0.0D;
+               if (dataObject.get("temperature") instanceof Long) {
+                  Long tempLong = (Long)dataObject.get("temperature");
+                  temperature = tempLong.doubleValue();
+               } else {
+                  temperature = (Double)dataObject.get("temperature");
+               }
 
-		    pf.setPaper(paper);
-		
-			/* Create an array of PrintServices */			
-			PrintService[] services = PrintServiceLookup.lookupPrintServices(null, null);			
-			/* Scan found services to see if anyone suits our needs */
-			PrintService labelPrinter = null;
-			for (int i = 0; i < services.length; i++) {
-				System.out.println("Printer: '" + services[i].getName()+"'");
-				//
-				//if (services[i].getName().contentEquals("\\\\ADMINRECEP\\DYMO LabelWriter Wireless")) {
-				if (services[i].getName().contentEquals(printer)) {
-					labelPrinter = services[i];
-					System.out.println("Found Printer");
-					break;
-				}
-			}
-		
+               if ((temperature != null) && (temperature >= 96.8D) && (temperature <= 99.5D)) {
+                  this.printPass(jo);
+               }
+            }
+         }
+      } catch (ParseException | IOException var8) {
+         var8.printStackTrace();
+      }
 
-			job.setPrintService(labelPrinter);
-			job.setPrintable(passable,pf);
-			//job.setPrintable(passable);			
-			job.print(attributes);
-		} catch (PrinterException ex) {
-			/* The job did not successfully complete */
-		}
-	}	
-	
-	public static void main(String[] args) {
-		WebSocketContainer container = null;//
-		Session session = null;
-				
-		if (args.length < 6) {
-			System.out.println("Usage:  java -jar LabelPrinter.jar -h(ost) <host> -p(ort) <port> -printer <printer name>");
-			System.exit(1);
-		}
-		String host="10.20.2.248";
-		String port="9000";
-		
-		try {
-			
-			// Create GNU like options
-			Options opts = new Options();
-			opts.addOption("h", "host", true, "Host")
-			 	.addOption("p", "port", true, "Port")
-				.addOption("P", "printer", true, "Printer");
-			
-			CommandLineParser parser = new DefaultParser();
-			CommandLine cmd = parser.parse(opts, args);
-			host = cmd.getOptionValue("host");
-			port = cmd.getOptionValue("port");
-			printer = cmd.getOptionValue("printer");
-			
-			System.out.println("GNU Host='"+host+"'");
-			System.out.println("GNU Port='"+port+"'");
-			System.out.println("GNU Printer='"+printer+"'");
-			
-			// security needed to access the device.  Per documentation the access hash is
-			// timestamp=<timestamp>&sign=MD5(timestamp#password)
-			String timestamp;
-			timestamp = "" + Calendar.getInstance().getTime().getTime();
-			String sign;
-			String password = "123456";
-			String remark = "remark=test";
-			sign = timestamp + "#" + password;
-			String md5Sign = md5Java(sign);
+   }
 
-			System.out.println("timestamp=" + timestamp);
-			System.out.println("sign='" + sign + "'");
-			System.out.println("md5Sign='" + md5Sign + "'");
+   private static void wait4TerminateSignal() {
+      synchronized(waitLock) {
+         try {
+            waitLock.wait();
+         } catch (InterruptedException var2) {
+         }
 
-			// BaseURL for the device
-			//String baseURL = "ws://10.20.2.248:9000/api/record?timestamp=";
-			String baseURL = "ws://"+host+":"+port+"/api/record?timestamp=";
-			String request = baseURL + timestamp + "&sign=" + md5Sign;// +"&"+remark;
-			System.out.println("Request='" + request + "'");
+      }
+   }
 
-			container = ContainerProvider.getWebSocketContainer();
-			//session = container.connectToServer(WSClient.class, URI.create("ws://10.20.2.248:9000/api/record?timestamp=1599743858281&sign=cd4bc5c543c184551fd3dc548acde02d"));			
-			session = container.connectToServer(WSClient.class, URI.create(request));
-			wait4TerminateSignal();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (session != null) {
-				try {
-					session.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-		
-	// Helper function needed to create password hash for the
-	// device.
-	public static String md5Java(String message) {
-		String digest = null;
-		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			byte[] hash = md.digest(message.getBytes("UTF-8"));
+   private void printPass(JSONObject jo) {
+      try {
+         PrinterJob job = PrinterJob.getPrinterJob();
+         PageFormat pf = job.defaultPage();
+         Paper paper = pf.getPaper();
+         new PrintPassJob2(jo);
+         PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
+         boolean zdPrinter = printer.contains("ZD420");
+         Object passable;
+         PrinterResolution pr;
+         if (!zdPrinter) {
+            passable = new PrintPassJob2(jo);
+            paper.setSize(72.0D, 249.84D);
+            paper.setImageableArea(7.2D, 7.2D, 72.0D, 249.84D);
+            attributes.add(OrientationRequested.LANDSCAPE);
+            pr = new PrinterResolution(313, 313, 100);
+            attributes.add(pr);
+         } else {
+            passable = new PrintPassZebraJob(jo);
+            paper.setSize(216.0D, 80.64D);
+            paper.setImageableArea(0.0D, 0.0D, 216.0D, 72.0D);
+            pr = new PrinterResolution(203, 203, 100);
+            attributes.add(OrientationRequested.PORTRAIT);
+            attributes.add(pr);
+         }
 
-			// converting byte array to Hexadecimal
-			StringBuilder sb = new StringBuilder(2 * hash.length);
-			for (byte b : hash) {
-				sb.append(String.format("%02x", b & 0xff));
-			}
-			digest = sb.toString();
-		} catch (UnsupportedEncodingException ex) {
-			Logger.getLogger(ex.getClass().getName()).log(Level.SEVERE, null, ex);
-		} catch (NoSuchAlgorithmException ex) {
-			Logger.getLogger(ex.getClass().getName()).log(Level.SEVERE, null, ex);
-		}
-		return digest;
-	}
+         pf.setPaper(paper);
+         PrintService[] services = PrintServiceLookup.lookupPrintServices((DocFlavor)null, (AttributeSet)null);
+         PrintService labelPrinter = null;
+
+         for(int i = 0; i < services.length; ++i) {
+            System.out.println("Printer: '" + services[i].getName() + "'");
+            if (services[i].getName().contentEquals(printer)) {
+               labelPrinter = services[i];
+               System.out.println("Found Printer");
+               break;
+            }
+         }
+
+         job.setPrintService(labelPrinter);
+         job.setPrintable((Printable)passable, pf);
+         job.print(attributes);
+      } catch (PrinterException var11) {
+      }
+
+   }
+
+   public static void main(String[] args) {
+      WebSocketContainer container = null;
+      Session session = null;
+      if (args.length < 6) {
+         System.out.println("Usage:  java -jar LabelPrinter.jar -h(ost) <host> -p(ort) <port> -printer <printer name>");
+         System.exit(1);
+      }
+
+      String host = "10.20.2.248";
+      String port = "9000";
+
+      try {
+         Options opts = new Options();
+         opts.addOption("h", "host", true, "Host").addOption("p", "port", true, "Port").addOption("P", "printer", true, "Printer");
+         CommandLineParser parser = new DefaultParser();
+         CommandLine cmd = parser.parse(opts, args);
+         host = cmd.getOptionValue("host");
+         port = cmd.getOptionValue("port");
+         printer = cmd.getOptionValue("printer");
+         System.out.println("GNU Host='" + host + "'");
+         System.out.println("GNU Port='" + port + "'");
+         System.out.println("GNU Printer='" + printer + "'");
+         String timestamp = "" + Calendar.getInstance().getTime().getTime();
+         String password = "123456";
+         String remark = "remark=test";
+         String sign = timestamp + "#" + password;
+         String md5Sign = md5Java(sign);
+         System.out.println("timestamp=" + timestamp);
+         System.out.println("sign='" + sign + "'");
+         System.out.println("md5Sign='" + md5Sign + "'");
+         String baseURL = "ws://" + host + ":" + port + "/api/record?timestamp=";
+         String request = baseURL + timestamp + "&sign=" + md5Sign;
+         System.out.println("Request='" + request + "'");
+         container = ContainerProvider.getWebSocketContainer();
+         session = container.connectToServer(WSClient.class, URI.create(request));
+         wait4TerminateSignal();
+      } catch (Exception var23) {
+         var23.printStackTrace();
+      } finally {
+         if (session != null) {
+            try {
+               session.close();
+            } catch (Exception var22) {
+               var22.printStackTrace();
+            }
+         }
+
+      }
+
+   }
+
+   public static String md5Java(String message) {
+      String digest = null;
+
+      try {
+         MessageDigest md = MessageDigest.getInstance("MD5");
+         byte[] hash = md.digest(message.getBytes("UTF-8"));
+         StringBuilder sb = new StringBuilder(2 * hash.length);
+         byte[] var8 = hash;
+         int var7 = hash.length;
+
+         for(int var6 = 0; var6 < var7; ++var6) {
+            byte b = var8[var6];
+            sb.append(String.format("%02x", b & 255));
+         }
+
+         digest = sb.toString();
+      } catch (UnsupportedEncodingException var9) {
+         Logger.getLogger(var9.getClass().getName()).log(Level.SEVERE, (String)null, var9);
+      } catch (NoSuchAlgorithmException var10) {
+         Logger.getLogger(var10.getClass().getName()).log(Level.SEVERE, (String)null, var10);
+      }
+
+      return digest;
+   }
 }
